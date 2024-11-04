7905fdc89c44133c0142bf419bbfa1478d122032
kkameoo <khdg1202@naver.com>
2024-11-04 11:45:41 +0900

    multuple if~else

--- src/com/javaex/basic/decisions/IfDecisionEx.java
+++ src/com/javaex/basic/decisions/IfDecisionEx.java
@@ -6,7 +6,7 @@ public class IfDecisionEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			ifEx();
+			ifEx2();
 	}
 	
 	private static void ifEx() {
@@ -19,8 +19,23 @@ public class IfDecisionEx {
 		} else {
 			System.out.println("불합격입니다.");
 		}
-		
 		scanner.close();
 	}
 	
+	private static void ifEx2() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요.");
+		int num = scanner.nextInt();
+		
+		if (num == 0) { 
+		   System.out.println("0 입니다. ");
+		} else {
+			if(num > 0) {
+				System.out.println("양수입니다.");
+			} else {
+				System.out.println("음수입니다.");
+			}
+		}
+		scanner.close();
+	}
 }
