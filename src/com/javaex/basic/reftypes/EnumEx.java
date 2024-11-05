59c8f271229c5740916d4be74c7d365b5e5c8512
kkameoo <khdg1202@naver.com>
2024-11-05 13:04:30 +0900

    ENUM COMMIT2

--- src/com/javaex/basic/reftypes/EnumEx.java
+++ src/com/javaex/basic/reftypes/EnumEx.java
@@ -1,10 +1,66 @@
 package com.javaex.basic.reftypes;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class EnumEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+//		usingEnum();
+		switchEnumPractice();
+	}
+	
+	private static void usingEnum() {
+		Week today = Week.TUESDAY;
+		
+		// 열거 상수  -> String
+		System.out.printf("Today is %s(%d)%n", today.name(), today.ordinal());
+		
+		String dayStr = "FRIDAY";
+		// String -> 열거상수 Convert
+		
+		Week obj = Week.valueOf(dayStr);
+		System.out.printf(obj.name() + " " + obj.ordinal()+"\n");
+		
+		// 열거 상수 목록
+		Week obj2[] = Week.values();
+		System.out.println(obj2);
+		for (int i = 0; i < 7; i++) {
+			System.out.println(obj2[i]);
+		}
+		
+		// compareTo : 두 객체 사이의 순서를 비교하는 메서드
+		Week otherDay = Week.FRIDAY;
+		// TUESDAY 와 FRIDAY 비교
+		System.out.println(today.compareTo(otherDay));
 	}
 
+	private static void switchEnumPractice() {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("요일을 입력하세요.");
+//		String week =scanner.nextLine();
+		
+		Week day = Week.TUESDAY;
+		
+		switch (day) {
+			case SUNDAY :
+				System.out.println("휴식");
+				break;
+			case MONDAY, TUESDAY, WEDNESDAY, THURSDAY :
+				System.out.println("열공");
+		    	break;
+		    case FRIDAY :
+		    	System.out.println("불금");
+		    	break;
+		    case SATURDAY :
+		    	System.out.println("주말");
+		    	break;
+		    default :
+		    	System.out.println("규격에 맞게 입력하시오.");
+		    	break;
+		}
+//		scanner.close();
+		
+	}
 }
--- src/com/javaex/practice/Practice03.java
+++ src/com/javaex/practice/Practice03.java
@@ -8,7 +8,8 @@ public class Practice03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		ex06();
-		ex07();
+//		ex07();
+		ex09();
 	}
 	
 	private static void ex06() {
@@ -31,5 +32,25 @@ public class Practice03 {
 			System.out.println();
 		}
 	}
+	
+	private static void ex08() {
+		for(int i = 1; i < 10; i++) {
+			for (int j = 2; j < 10; j++) {
+				System.out.print(j+"*"+i+ "="+(i * j) + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void ex09() {
+		for(int i = 0; i < 10; i++) {
+			for (int j = 1 + i; j < 11 + i; j++) {
+				System.out.print(j + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	
 
 }
