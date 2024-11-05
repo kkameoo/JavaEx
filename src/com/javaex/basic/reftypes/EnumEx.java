b52cb7fc512f66f7994a0872fc843d9146baf3fb
kkameoo <khdg1202@naver.com>
2024-11-05 12:17:07 +0900

    ENUM COMMIT1

---
+++ src/com/javaex/basic/reftypes/EnumEx.java
@@ -0,0 +1,10 @@
+package com.javaex.basic.reftypes;
+
+public class EnumEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	}
+
+}
---
+++ src/com/javaex/basic/reftypes/Week.java
@@ -0,0 +1,12 @@
+package com.javaex.basic.reftypes;
+
+public enum Week {
+	// 열거형 객체 생성
+		SUNDAY,
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY
+}
