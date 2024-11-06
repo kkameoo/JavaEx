a75c89cd1da0b16642316d1b036062092718dfca
kkameoo <khdg1202@naver.com>
2024-11-06 10:55:24 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/com/javaex/basic/reftypes/MainArgs.java
@@ -0,0 +1,12 @@
+package com.javaex.basic.reftypes;
+
+import java.util.Arrays;
+
+public class MainArgs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("전달된 매개 변수:" + Arrays.toString(args));
+	}
+
+}
