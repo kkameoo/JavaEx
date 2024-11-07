bfd793190a17871cb0f00ec7e4dc53df11e90190
kkameoo <khdg1202@naver.com>
2024-11-07 18:10:45 +0900

    COMMIT LAST

---
+++ src/com/javaex/practice05/ex10/Book.java
@@ -0,0 +1,24 @@
+package com.javaex.practice05.ex10;
+
+public class Book {
+	 private int bookNo;
+	 private String title;
+	 private String author;
+	 private int stateCode;
+	 
+	 public Book(int bookNo, String title, String author) {
+		// TODO Auto-generated constructor stub
+		 this.bookNo =bookNo;
+		 this.title =title;
+		 this.author =author;
+	}
+	 
+	 public void rent() {
+		
+	}
+	 
+	 public void print() {
+		
+	}
+	 
+}
