c2eae2e5a0bb24242f8ac160c26c113e41a3e876
kkameoo <khdg1202@naver.com>
2024-11-07 10:20:09 +0900

    SUMVAR COMMIT

--- src/com/javaex/oop/MethodEx.java
+++ src/com/javaex/oop/MethodEx.java
@@ -1,16 +1,43 @@
 package com.javaex.oop;
 
+import java.util.Iterator;
+
 public class MethodEx {
 
 	// 메서드 작성 연습
 	public static void main(String[] args) {
-		printMessage();
-		String message = getMessage();
-		String message2 = "매개 변수 o, 리턴 x";
-		String message3 = "매개 변수 o, 리턴 o";
-		System.out.println(message);
-		setMessage(message2);
-		System.out.println(printMessage(message3));
+//		printMessage();
+//		String message = getMessage();
+//		String message2 = "매개 변수 o, 리턴 x";
+//		String message3 = "매개 변수 o, 리턴 o";
+//		System.out.println(message);
+//		setMessage(message2);
+//		System.out.println(printMessage(message3));
+		
+		// 매개변수 개수를 알 수 없을때 해결방법
+		// 방법1, 배열을 전달한다
+		System.out.println("getSumArr : " + getSumArr(new double[] { 1, 2, 3, 4, 5, 6, 7, 8}));
+		// 방법2, 가변 인수를 적용
+		System.out.println("getSumVar : " + getSumVar(1, 2, 3, 4, 5, 6, 7, 8));
+	}
+	
+	private static double getSumVar(double ... values) {
+		double total = 0;
+		
+		for (double value: values) {
+			total += value;
+		}
+		return total;
+	}
+	
+	private static double getSumArr(double[] values) {
+		double total = 0;
+		
+		for (int i = 0; i < values.length; ++i) {
+			total += values[i];
+		}
+		
+		return total;
 	}
 	
 	private static void printMessage() {
--- src/com/javaex/types/Practice.java
+++ src/com/javaex/types/Practice.java
@@ -1,18 +1,26 @@
 package com.javaex.types;
 
+import java.util.Scanner;
+
 public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Point a = new Point(2, 3);
-		Point b = new Point(2, 3);
+//		Point a = new Point(2, 3);
+//		Point b = new Point(2, 3);
 //		System.out.println( a == b );
 //		System.out.println( a.equals( b ) );
 //		String s1 = new String("hello");
 //		String s2 = new String("hello");
 //		System.out.println( s1 == s2 );
 //		System.out.println( s1.equals( s2 ) );
-		System.out.println( a == b );
-		System.out.println( a.equals( b ) );
+//		System.out.println( a == b );
+//		System.out.println( a.equals( b ) );
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		double c = a + b;
+		System.out.println(c);
+		scanner.close();
 	}
 }
