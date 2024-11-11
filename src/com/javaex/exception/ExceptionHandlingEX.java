731b063b49049135dd468a717378a70507e7c6d8
kkameoo <khdg1202@naver.com>
2024-11-11 11:22:20 +0900

    TOSTRING COMMIT

--- src/com/javaex/exception/ExceptionHandlingEX.java
+++ src/com/javaex/exception/ExceptionHandlingEX.java
@@ -7,7 +7,35 @@ public class ExceptionHandlingEX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		arithExceptionEx();
+//		arithExceptionEx();
+		arrayExceptionEx();
+		nullPointerExceptionEx();
+		
+	}
+	
+	private static void arrayExceptionEx() {
+		int intArray[] = new int[] {3, 6, 9};
+		
+		
+		try {
+			System.out.println(intArray[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// TODO: handle exception
+			System.err.println("Error : " + e.getMessage() );
+		}
+	}
+	
+	private static void nullPointerExceptionEx() {
+		String str = new String("Hello Java");
+		try {
+			str = null;
+			System.out.println(str.toUpperCase());
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+			System.err.println("Error : " + e.getMessage() );
+		}
+		
+		
 	}
 	
 	private static void arithExceptionEx() {
@@ -32,7 +60,5 @@ public class ExceptionHandlingEX {
 			System.out.println("프로그램 종료");
 		}
 		scanner.close();
-		
 	}
-
 }
